861e0ca7e48a3f678461e38cdac005e201acae3e
Sirlande Martins <sirlande09@gmail.com>
2026-05-14 21:36:15 -0300

    Últimos Ajustes

--- routes/apis/courseRouter.js
+++ routes/apis/courseRouter.js
@@ -1,9 +1,23 @@
 import { Router } from 'express';
 
+import ListCourseController from '../../app/Controllers/CourseApi/ListCourseController.js';
+import GetCourseController from '../../app/Controllers/CourseApi/GetCourseController.js';
+import CreateCourseController from '../../app/Controllers/CourseApi/CreateCourseController.js';
+import UpdateCourseController from '../../app/Controllers/CourseApi/UpdateCourseController.js';
+import DeleteCourseController from '../../app/Controllers/CourseApi/DeleteCourseController.js';
+
 export default (() => {
     const router = Router();
 
-    /** TF 11 */
+    router.get('/', ListCourseController);
+
+    router.get('/:id', GetCourseController);
+
+    router.post('/', CreateCourseController);
+
+    router.put('/:id', UpdateCourseController);
+
+    router.delete('/:id', DeleteCourseController);
 
     return router;
 })();
